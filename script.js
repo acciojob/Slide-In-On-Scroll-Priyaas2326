3245df35f57118be9a2a28e89b2fc9d1ce4bd29f
Priya Sharma <113659062+Priyaas2326@users.noreply.github.com>
2023-02-13 22:13:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 // Your JS code here.
+  function slideInOnScroll() {
+    const images = document.querySelectorAll('.slide-in');
+
+    images.forEach(image => {
+      const slideInAt = (window.scrollY + window.innerHeight) - image.height / 2;
+      const imageBottom = image.offsetTop + image.height;
+      const isHalfShown = slideInAt > image.offsetTop;
+      const isNotScrolledPast = window.scrollY < imageBottom;
+
+      if (isHalfShown && isNotScrolledPast) {
+        image.classList.add('active');
+      } else {
+        image.classList.remove('active');
+      }
+    });
+  }
+
+  function debounce(func, wait = 20, immediate = true) {
+    let timeout;
+    return function() {
+      let context = this, args = arguments;
+      let later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      let callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+
+  window.addEventListener('scroll', debounce(slideInOnScroll));
+
+
