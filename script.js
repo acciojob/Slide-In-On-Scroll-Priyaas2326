4eb0210b80af6df77cb6f2b02d8538b2aa04e88d
Priya Sharma <113659062+Priyaas2326@users.noreply.github.com>
2023-02-13 22:32:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,19 @@
-// Your JS code here.
-  function slideInOnScroll() {
-    const images = document.querySelectorAll('.slide-in');
+const sliderImages = document.querySelectorAll('.slide-in');
 
-    images.forEach(image => {
-      const slideInAt = (window.scrollY + window.innerHeight) - image.height / 2;
-      const imageBottom = image.offsetTop + image.height;
-      const isHalfShown = slideInAt > image.offsetTop;
-      const isNotScrolledPast = window.scrollY < imageBottom;
+        function checkSlide() {
+            sliderImages.forEach(sliderImage => {
+                // half way through the image
+                const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+                // bottom of the image
+                const imageBottom = sliderImage.offsetTop + sliderImage.height;
+                const isHalfShown = slideInAt > sliderImage.offsetTop;
+                const isNotScrolledPast = window.scrollY < imageBottom;
+                if (isHalfShown && isNotScrolledPast) {
+                    sliderImage.classList.add('active');
+                } else {
+                    sliderImage.classList.remove('active');
+                }
+            });
+        }
 
-      if (isHalfShown && isNotScrolledPast) {
-        image.classList.add('active');
-      } else {
-        image.classList.remove('active');
-      }
-    });
-  }
-
-  function debounce(func, wait = 20, immediate = true) {
-    let timeout;
-    return function() {
-      let context = this, args = arguments;
-      let later = function() {
-        timeout = null;
-        if (!immediate) func.apply(context, args);
-      };
-      let callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
-    };
-  };
-
-  window.addEventListener('scroll', debounce(slideInOnScroll));
-
-
+        window.addEventListener('scroll', debounce(checkSlide));
